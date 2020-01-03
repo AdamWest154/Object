cf9017aedcc772cd743cc57a5d189565372ca5f6
Erik <erik.martinez@s2grupo.es>
2020-01-03 21:03:46 +0100

    Second

--- go.mod
+++ go.mod
@@ -1,9 +1,5 @@
-module "https://github.com/AdamWest154/Object"
+module github.com/AdamWest154/Object
 
 go 1.12
 
-require (
-	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
-	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	gopkg.in/tucnak/telebot.v2 v2.0.0-20191225234705-baa616bc00d5 // indirect
-)
+require gopkg.in/tucnak/telebot.v2 v2.0.0-20191225234705-baa616bc00d5 // indirect
--- main.go
+++ main.go
@@ -1,27 +1,6 @@
 package main
 
 import (
-    "os"
-    tb "gopkg.in/tucnak/telebot.v2"
-)
-
-func main() {
-    var (
-        port      = os.Getenv("PORT")       // sets automatically
-        publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
-        token     = os.Getenv("TOKEN")      // you must add it to your config vars
-    )
-
-    webhook := &tb.Webhook{
-        Listen:   ":" + port,
-        Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
-    }
-}
-Configuring telebot
-After webhook being set up we can create an instance of our bot. Here we use our webhook as a Poller, a thing that looks for updates.
-package main
-
-import (
     "log"
     "os"
 
@@ -52,6 +31,6 @@ func main() {
 
 
     b.Handle("/hello", func(m *tb.Message) {
-    b.Send(m.Sender, "Hi!")
+        b.Send(m.Sender, "Hi!")
     })
 }
